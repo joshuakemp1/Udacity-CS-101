f8dc823fbe34ff1ab790368965704677041a9955
Joshua Kemp <josh.kemp@ziplist.com>
2014-01-01 15:52:29 -0500

    quiz 1

---
+++ cs101_lesson1_quiz1.rb
@@ -0,0 +1,22 @@
+# Write Python code that prints out Udacity (with a capital U), 
+# given the definition of s below.
+# Here is that answer in Python 
+
+# s = 'audacity'
+    
+    
+#print 'U' + s[2:] 
+
+# And here is the same answer in Ruby
+
+s = 'audacity'
+
+puts 'U' + s[2..-1]
+
+#or
+
+puts s[1..-1].capitalize
+
+#or
+
+puts s[1..8].capitalize
