cab307ad3ec1fad3f3ebf647d941a6b150e42e14
Joshua Kemp <josh.kemp@ziplist.com>
2014-01-29 21:55:18 -0500

    Fun exercise that shows why Ruby is so fun :-)

---
+++ biggest.rb
@@ -0,0 +1,16 @@
+def biggest(n1,n2,n3)
+  total_num = n1,n2,n3
+  total_num.min
+
+end
+
+puts biggest(3,6,9)
+
+# puts biggest(6,9,3)
+
+# puts biggest(9,3,6)
+
+# puts biggest(3,3,9)
+
+# puts biggest(9,3,9)
+
