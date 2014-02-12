9780aae382807de5be1a6aa8c458bd6eeb264049
Joshua Kemp <josh.kemp@ziplist.com>
2014-02-12 00:06:28 -0500

    method that parses string for URL and last quote

--- second_crawler.rb
+++ second_crawler.rb
@@ -12,13 +12,17 @@
 def get_next_target(page)
     start_link = page.index('<a href=')#found position 10
 
-
-    #Insert your code below here
+    if start_link == nil
+      puts  -1
+    else
+       start_link
 
    start_quote = page.index('"', start_link)#found position 18
    end_quote = page.index('"', start_quote + 1)#found position 37
    url = page[start_quote..end_quote]
    return url, end_quote
   end
+end
 
-  puts get_next_target('this is a <a href="http://udacity.com">link!</a>')
+    puts get_next_target('this is a <a href="http://udacity.com">link!</a>')
+    # puts get_next_target('billy bob')
