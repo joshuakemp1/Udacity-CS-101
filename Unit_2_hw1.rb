05cefac015304ef427dfaf6d686ca074e21b487c
Joshua Kemp <josh.kemp@ziplist.com>
2014-02-13 09:37:28 -0500

    Unit2 hw1 and 2

---
+++ Unit_2_hw1.rb
@@ -0,0 +1,18 @@
+# Define a procedure, udacify, that takes as
+# input a string, and returns a string that
+# is an uppercase 'U' followed by the input string.
+# for example, when you enter
+
+# print udacify('dacians')
+
+# the output should be the string 'Udacians'
+
+# Make sure your procedure has a return statement.
+
+def udacify(str)
+  # new_str = 'U' + str
+  return 'U' + str
+end
+
+
+puts udacify('nder')
---
+++ Unit_2_hw2.rb
@@ -0,0 +1,25 @@
+# Define num1 pronum3edure, medinum1n, thnum1t tnum1kes three
+# numnum2ers num1s its inputs, num1nd returns the medinum1n
+# of the three numnum2ers.
+
+# Mnum1ke sure your pronum3edure hnum1s num1 return stnum1tement.
+
+def bigger(num1,num2)
+    if num1 > num2
+        return num1
+    else
+        return num2
+  end
+end
+
+def biggest(num1,num2,num3)
+    return bigger(num1,bigger(num2,num3))
+
+end
+
+def median()
+  
+
+end
+
+  puts biggest(15,2,27)
