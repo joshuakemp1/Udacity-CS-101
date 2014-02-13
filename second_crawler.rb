08751ebd697fca6017b1be6d8a43f836a598765e
Joshua Kemp <josh.kemp@ziplist.com>
2014-02-13 09:11:34 -0500

    Tweaking second_crawler

--- second_crawler.rb
+++ second_crawler.rb
@@ -9,20 +9,46 @@
 # Also note that your answer will appear in
 # parentheses if you print it.
 
+
 def get_next_target(page)
     start_link = page.index('<a href=')#found position 10
 
     if start_link == nil
-      puts  -1
+      return nil, -1
     else
        start_link
 
    start_quote = page.index('"', start_link)#found position 18
    end_quote = page.index('"', start_quote + 1)#found position 37
-   url = page[start_quote..end_quote]
+   url = page[start_quote +1...end_quote]
    return url, end_quote
   end
 end
 
-    puts get_next_target('this is a <a href="http://udacity.com">link!</a>')
-    # puts get_next_target('billy bob')
+      print get_next_target('this is a <a href="http://udacity.com">link!</a>')
+      # print get_next_target('billy bob')
+
+def print_all_links(page)
+   while true
+    url, end_position = get_next_target(page)
+    if url == get_next_target(page)
+      print url
+      page = page[end_position]
+    else
+      break
+    end
+  end
+end
+
+
+
+ page = ('this is a <a href="http://joshuakemp.net">link!</a>this is a <a href="http://gratefulwithtwo.com">link!</a>')
+
+ url, end_position = get_next_target(page)
+
+ print url
+
+ page = page[end_position]
+
+
+
