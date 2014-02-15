250030256a2e5ee023e2d0cdb378ab6cb861d7c6
Joshua Kemp <josh.kemp@ziplist.com>
2014-02-15 10:54:31 -0500

    Udacity Unit 2 homework

---
+++ median.rb
@@ -0,0 +1,32 @@
+def larger(num1,num2)#then here
+  if num1 > num2
+    return num1
+  else
+    return num2
+  end
+end
+
+
+
+
+def largest(num1,num2,num3)#calls here
+  return larger(num1,larger(num2,num3))#then back again
+end
+
+
+
+
+def median(num1,num2,num3)#then here
+  highest_num = largest(num1,num2,num3)
+   if  highest_num == num1
+     return larger(num2,num3)
+  elsif highest_num == num2
+    return larger(num1,num3)
+  else
+    return larger(num1,num2)
+ end
+end
+
+
+# puts median(largest)
+puts median(44,12,66)
